2086949d5a05d345049746ad1d008a3dfbd73111
Qic@Mac <zqc007@gmail.com>
2021-02-01 16:06:38 +0800

    ✨feat(c): build

--- src/index.c
+++ src/index.c
@@ -1,5 +1,7 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
 
 #include <memory.h>
+#include <emscripten.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -61,6 +63,10 @@ unsigned char PADDING[] =
 	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 };
+
+EM_PORT_API(int) add(int a, int b) {
+  return a + b;
+}
 
 void MD5Init(MD5_CTX *context)
 {
@@ -235,29 +241,9 @@ void MD5Transform(unsigned int state[4], unsigned char block[64])
 }
 // function declare
 int Compute_string_md5(unsigned char *dest_str, unsigned int dest_len, char *md5_str);
-int Compute_file_md5(const char *file_path, char *md5_str);
 
-/************** main test **************/
-int main(int argc, char *argv[])
-{
-	char md5_str[MD5_STR_LEN + 1];
-	const char *test_str = "1234";
 
-	// test string md5 
-	Compute_string_md5((unsigned char *)test_str, strlen(test_str), md5_str);
-	printf("[string - %s] md5 value:\n", test_str);
-	printf("%s\n", md5_str);
-
-	return 0;
-}
-
-/**
- * compute the value of a string
- * @param  dest_str
- * @param  dest_len
- * @param  md5_str
- */
-int Compute_string_md5(unsigned char *dest_str, unsigned int dest_len, char *md5_str)
+EM_PORT_API(int) Compute_string_md5(unsigned char *dest_str, unsigned int dest_len, char *md5_str)
 {
 	int i;
 	unsigned char md5_value[MD5_SIZE];
